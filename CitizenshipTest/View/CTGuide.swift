819abc98032110b3017005ce76a2f5e0ae91f553
Khang Tran <khangmt147@gmail.com>
2025-01-31 18:36:26 -0700

    Change LearnQuestions screen with new drop down menu for part and added guide screen before starting

---
+++ CitizenshipTest/View/CTGuide.swift
@@ -0,0 +1,18 @@
+//
+//  CTGuide.swift
+//  CitizenshipTest
+//
+//  Created by Khang Tran on 1/31/25.
+//
+
+import SwiftUI
+
+struct CTGuide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CTGuide()
+}
